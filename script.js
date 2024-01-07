49e1fe22265148720846dcbd0b281bb608fce1b4
Ekow Perry <107459827+YoPerry007@users.noreply.github.com>
2024-01-07 00:06:06 +0000

    Update script.js by adding actual codes cus the first commit was an empty file

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector(".separator-img");
+const inputs = document.getElementsByTagName("input");
+const ageOutputs = document.getElementsByTagName("span");
+let isValid = false;
