346e5afc5e48b110e7dced3ec38d659866645099
Ekow Perry <107459827+YoPerry007@users.noreply.github.com>
2024-01-08 14:43:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ const btn = document.querySelector(".separator-img");
 const inputs = document.getElementsByTagName("input");
 const ageOutputs = document.getElementsByTagName("span");
 let isValid = false;
+
+
+btn.addEventListener(
+  "click",
+  (calculateAge = () => {
+    // if (isValid) {
+    let actualAge = `${inputs[1].value}/${inputs[0].value}/${inputs[2].value}`;
+    let ageObj = new Date(actualAge);
+    let ageDifference = Date.now() - ageObj;
+    ageDate = new Date(ageDifference);
+
+    let yearsOfAge = ageDate.getUTCFullYear() - 1970;
+    let monthsOfAge = ageDate.getUTCMonth();
+    let daysOfAge = ageDate.getUTCDay() - 1;
+    ageOutputs[0].textContent = yearsOfAge;
+    ageOutputs[1].textContent = monthsOfAge;
+    ageOutputs[2].textContent = daysOfAge;
+    // }
+    //  else {
+    //   alert("Error! Your date seems incorrect");
+    // }
+  })
+);
